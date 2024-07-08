f5676250ec51d83c9ba04206f89c8a50a2e7d233
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2024-07-09 06:52:28 +0700

    Commit Week 3 (Addition)

--- app/src/main/java/com/example/hotelku/Reservation.java
+++ app/src/main/java/com/example/hotelku/Reservation.java
@@ -1,6 +1,14 @@
 package com.example.hotelku;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class Reservation extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+    }
 }
